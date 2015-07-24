f006db56f5b82b98d5d714f6a11a73fbd38a0aaf
supistar <supistars@gmail.com>
2015-07-24 18:42:20 +0900

    Add init.sh script to support RedHat environment installation.

---
+++ scripts/distributions/RedHat/init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Update yum
+sudo yum update
+
+# Install basic packages
+sudo yum install -y git patch
+# Install packages for building ruby
+sudo yum install -y readline-devel
+# Install packages for building python
+sudo yum install -y gmp gmp-devel
+
+# Set anyenv path
+sh ./scripts/set-anyenv-path.sh bash_profile
+# Reload profile
+source ~/.bash_profile
+# Install anyenv
+sh ./scripts/install-anyenv.sh
+
+# Update pip
+pip install --upgrade pip
+# Install ansible
+pip install ansible
+
+# Execute ansible
+ansible-playbook redhat.yml -i hosts
+
