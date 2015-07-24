71daf6d6217c1c188045af5c1ba48a9e7ab4fdb3
supistar <supistars@gmail.com>
2015-07-24 18:40:11 +0900

    Move existing init.sh script under distributions/Mac directory.

---
+++ scripts/distributions/Mac/init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Agree Xcode llcense
+sudo xcodebuild -license
+
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+
+# Update homebrew
+brew install git
+brew update
+
+# Install ansible and roles
+brew install ansible
+ansible-galaxy install hnakamur.homebrew-packages
+ansible-galaxy install hnakamur.homebrew-cask-packages
+
+# Set anyenv path
+sh ./scripts/set-anyenv-path.sh bash_profile
+# Reload profile
+source ~/.bash_profile
+# Install anyenv
+sh ./scripts/install-anyenv.sh
+
+# Execute ansible
+ansible-playbook macbook.yml -i hosts
+
