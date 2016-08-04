45d0609e53a7c7f82c29b20a92f012d6913dae9d
supistar <supistars@gmail.com>
2016-08-04 15:36:29 +0900

    Fix anyenv installation script to reload shell before installation.

--- scripts/install-anyenv.sh
+++ scripts/install-anyenv.sh
@@ -16,27 +16,18 @@ eval "$(anyenv init -)"
 IS_INSTALLED_RB=$(rbenv version | grep ${RB_VER} | wc -l | xargs echo)
 if [ ${IS_INSTALLED_RB} -eq 0 ]; then
     anyenv install -f rbenv
-    exec ${SHELL} -l
-    rbenv install -f ${RB_VER}
-    rbenv global ${RB_VER}
-    rbenv rehash
+    ${SHELL} -lc "rbenv install -f ${RB_VER} && rbenv global ${RB_VER} && rbenv rehash"
 fi
 
 IS_INSTALLED_ND=$(ndenv version | grep ${ND_VER} | wc -l | xargs echo)
 if [ ${IS_INSTALLED_ND} -eq 0 ]; then
     anyenv install -f ndenv
-    exec ${SHELL} -l
-    ndenv install -f ${ND_VER}
-    ndenv global ${ND_VER}
-    ndenv rehash
+    ${SHELL} -lc "ndenv install -f ${ND_VER} && ndenv global ${ND_VER} && ndenv rehash"
 fi
 
 IS_INSTALLED_PY=$(pyenv version | grep ${PY_VER} | wc -l | xargs echo)
 if [ ${IS_INSTALLED_PY} -eq 0 ]; then
     anyenv install -f pyenv
-    exec ${SHELL} -l
-    pyenv install -f ${PY_VER}
-    pyenv global ${PY_VER}
-    pyenv rehash
+    ${SHELL} -lc "pyenv install -f ${PY_VER} && pyenv global ${PY_VER} && pyenv rehash"
 fi
 
