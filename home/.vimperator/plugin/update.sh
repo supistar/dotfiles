d134ee47fe9f5d9d1b343a290a2cf4bd785af21f
supistar <supistars@gmail.com>
2016-08-05 10:20:52 +0900

    Comment out unused vimperator plugins from update.sh script.

--- home/.vimperator/plugin/update.sh
+++ home/.vimperator/plugin/update.sh
@@ -3,17 +3,19 @@
 PLUGINS=( \
     "_libly.js" \
     "bitly.js" \
-    "asdfghjkl.js" \
     "copy.js" \
     "direct_bookmark.js" \
     "feedSomeKeys_3.js" \
     "hatenaStar.js" \
-    "migemized_find.js" \
-    "prevent_focus_ietab.js" \
     "sbmcommentsviewer.js" \
-    "xpath_hint.js" \
     "readitlater.js" \
     )
+#PLUGINS+=(
+#    "asdfghjkl.js" \
+#    "migemized_find.js" \
+#    "prevent_focus_ietab.js" \
+#    "xpath_hint.js" \
+#)
 
 for plugin in "${PLUGINS[@]}"; do
     wget -O ${plugin} https://raw.githubusercontent.com/vimpr/vimperator-plugins/master/${plugin} --no-check-certificate
